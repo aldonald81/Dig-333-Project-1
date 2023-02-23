09484a5151674d3555cb8489bf752cdea9032c25
Alexander <aldonald@davidson.edu>
2023-02-23 14:49:59 -0500

    a

--- audio.py
+++ audio.py
@@ -3,15 +3,15 @@ import bluetooth
 # Search for nearby Bluetooth devices
 devices = bluetooth.discover_devices()
 
-
-print(devices)
 # Print the names of the nearby devices
 for device in devices:
     print(device, bluetooth.lookup_name(device))
 
-"""
+
 # MAC address of the Bluetooth speaker
-speaker_addr = '6C:47:60:A9:C2:B8'
+#speaker_addr = '6C:47:60:A9:C2:B8' # JBL
+speaker_addr = '50:DE:06:F2:7E:7E' # Power Beats
+
 print(1)
 # Connect to the Bluetooth speaker
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -22,6 +22,3 @@ sock.send(b'Hello, world!')
 
 # Disconnect from the speaker
 sock.close()
-
-
-"""
