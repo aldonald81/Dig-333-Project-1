14b2efad58e9fe14cd654d66abcd08725e642c9e
Alexander <aldonald@davidson.edu>
2023-03-20 13:25:41 -0400

    a

--- main.py
+++ main.py
@@ -10,7 +10,46 @@ import pygame
 
 from gtts import gTTS
 from playsound import playsound
-#import vlc
+
+import RPi.GPIO as GPIO
+import time
+
+### READ VALUE FROM LDR
+mpin=17
+tpin=27
+GPIO.setmode(GPIO.BCM)
+cap=0.000001
+adj=2.130620985
+i=0
+t=0
+while t < 100000:
+    GPIO.setup(mpin, GPIO.OUT)
+    GPIO.setup(tpin, GPIO.OUT)
+    GPIO.output(mpin, False)
+    GPIO.output(tpin, False)
+    time.sleep(0.2)
+    GPIO.setup(mpin, GPIO.IN)
+    time.sleep(0.2)
+    GPIO.output(tpin, True)
+    starttime=time.time()
+    endtime=time.time()
+    while (GPIO.input(mpin) == GPIO.LOW):
+        endtime=time.time()
+    measureresistance=endtime-starttime
+    
+    res=(measureresistance/cap)*adj
+    i=i+1
+    t=t+res
+    if i==10:
+        t=t/i
+        print(t)
+        i=0
+        t=0
+    
+    # if resistance (t) is above some value, exit and trigger the alarm
+
+
+print("alarm")
 
 text = "Hello, how are you?"
 
